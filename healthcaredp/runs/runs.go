9c07da52f9278609250b79b40f87d65a4ec1e40b
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2025-02-20 09:51:08 +0100

    Added new operations to run from file function, now fully integrated with cli

--- healthcaredp/runs/runs.go
+++ healthcaredp/runs/runs.go
@@ -9,6 +9,8 @@ import (
 	"healthcaredp"
 	"healthcaredp/aggregations"
 	"healthcaredp/model"
+	"healthcaredp/utils"
+	"strings"
 )
 
 func RunAll(cmd *cobra.Command, args []string) (err error) {
@@ -143,6 +145,7 @@ func RunFromFile(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return fmt.Errorf("error loading config file: %v", err)
 	}
+
 	err = healthcaredp.Budget.InitYamlBudgetShares(config)
 	if err != nil {
 		return err
@@ -174,9 +177,41 @@ func RunFromFile(cmd *cobra.Command, args []string) (err error) {
 	}
 	pcol := model.ReadGenericInput(model.GlobalScope, newDFilename)
 
-	pColCount := aggregations.CountColumn(model.GlobalScope, pcol, "TestResults", healthcaredp.Budget)
+	for _, op := range config.PipelineDp.Operations {
+		if !utils.SliceContains(model.Headers, op.Column) {
+			return fmt.Errorf("column %s does not exists in the dataset", op.Column)
+		}
 
-	model.PrintConsole(model.GlobalScope, pColCount)
+		outFilename := fmt.Sprintf("%s/%s_%s.csv",
+			config.PipelineDp.Configuration.DataDir,
+			strings.TrimSuffix(config.PipelineDp.Configuration.OutputBaseName, ".csv"),
+			op.OperationName)
+
+		fmt.Printf("Results for %s operation saved in file %s\n\n", op.OperationName, outFilename)
+		switch op.OperationType {
+		case "count":
+			pColCount, err := aggregations.CountColumn(model.GlobalScope, pcol, op, healthcaredp.Budget)
+			if err != nil {
+				return fmt.Errorf("error calculating count: %v", err)
+			}
+			model.WriteOutput(model.GlobalScope, *pColCount, outFilename)
+			model.PrintConsole(model.GlobalScope, *pColCount)
+		case "mean_per_key":
+			pColMean, err := aggregations.MeanColumnByKey(model.GlobalScope, pcol, op, healthcaredp.Budget)
+			if err != nil {
+				return fmt.Errorf("error calculating mean: %v", err)
+			}
+			model.WriteOutput(model.GlobalScope, *pColMean, outFilename)
+			model.PrintConsole(model.GlobalScope, *pColMean)
+		case "sum_per_key":
+			pColSum, err := aggregations.SumColumnByKey(model.GlobalScope, pcol, op, healthcaredp.Budget)
+			if err != nil {
+				return fmt.Errorf("error calculating sum: %v", err)
+			}
+			model.WriteOutput(model.GlobalScope, *pColSum, outFilename)
+			model.PrintConsole(model.GlobalScope, *pColSum)
+		}
+	}
 
 	// Execute pipeline.
 	_, err = direct.Execute(context.Background(), model.GlobalPipeline)
